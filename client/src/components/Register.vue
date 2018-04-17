1b471587cd6f51ec0dac27fd3f612c6eb973bc88
55242 <738543046@qq.com>
2018-04-17 08:51:43 +0800

    The Register Function is done.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,24 +1,40 @@
 <template>
-  <div>
-    <h1>Register</h1>
-
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email"/>
-    <br>
-    <input
-      type="password"
-      name='password'
-      v-model="password"
-      placeholder="password"/>
-    <br>
-    <button
-      @click="register">
-    Register
-    </button>
-  </div>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="while elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            name="email"
+            label="Email"
+            v-model="email"
+            class="input-group--focused">
+          </v-text-field>
+          <br>
+          <v-text-field
+            name="password"
+            label="Enter your password"
+            hint="At least 8 characters"
+            min="8"
+            :append-icon="e3 ? 'visibility' : 'visibility_off'"
+            :append-icon-cb="() => (e3 = !e3)"
+            v-model="password"
+            class="input-group--focused"
+            :type="e3 ? 'password' : 'text'"
+          ></v-text-field>
+          <br>
+          <div v-html="error" class="error"/>
+          <br>
+          <v-btn
+            @click="register">
+          Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -27,16 +43,21 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null,
+      e3: true
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -44,4 +65,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  text-decoration-color: red;
+}
 </style>
